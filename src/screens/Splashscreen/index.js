3e5a7060f4ae384b1c4f56c279a94191a64d9769
AndrewRett <146518775+AndrewRett@users.noreply.github.com>
2024-04-29 20:17:05 +0800

    Update home and splashscreeb

--- src/screens/Splashscreen/index.js
+++ src/screens/Splashscreen/index.js
@@ -1,20 +1,33 @@
-import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {useEffect} from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const Splashscreen = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigation.navigate('Login');
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
   return (
-    <View>
-      <Text>Splash Screen</Text>
-    </View>
+    <LinearGradient
+      colors={['#50C5AEA6', '#F6EF40A6']}
+      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Image source={require('../../../assets/images/logo.png')} />
+    </LinearGradient>
   );
 };
 
 export default Splashscreen;
 
-const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#20a090',
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-});
+const styles = StyleSheet.create({});
