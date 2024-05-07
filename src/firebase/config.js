61c6ce124fff9493e5bfb6fcbce5dea3d2105983
OxanaMaindoka <s22110068@student.unklab.ac.id>
2024-05-07 14:52:33 +0800

    setup firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7z49AZaM7r-cjUpR_pGAa8Gl-zMkKXNU",
+  authDomain: "pesona-manado.firebaseapp.com",
+  projectId: "pesona-manado",
+  storageBucket: "pesona-manado.appspot.com",
+  messagingSenderId: "384803898422",
+  appId: "1:384803898422:web:4ffcedacb62fe507fb8497"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
